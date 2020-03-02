c08a886e7262259e2312d0fc302e49c399789c3d
Mauricio Ponce Oyarzún <mauricio.ponce.o@gmail.com>
2020-03-02 15:37:38 -0300

    [ADD] el adapter se asigna al recyclerview

--- app/src/main/java/cl/desafiolatam/postresfiness/MainActivity.java
+++ app/src/main/java/cl/desafiolatam/postresfiness/MainActivity.java
@@ -1,23 +1,34 @@
 package cl.desafiolatam.postresfiness;
 
+import android.os.Bundle;
+import android.util.Log;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
+
+    private RecipeFitAdapter.OnItemClickListener onItemClickListener = new RecipeFitAdapter.OnItemClickListener() {
+        @Override
+        public void onClick(RecipeFitAdapter.MyViewHolder myViewHolder, int idDessertFit) {
+            Log.d(TAG, "onClick entrando, método no implementado");
+        }
+    };
+
     @Override
-
-
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        RecyclerView mRecyclerView = findViewById(R.id.recycler_view);
+        mRecyclerView.setHasFixedSize(true);
+
         RecyclerView.LayoutManager gridLayoutManager = new GridLayoutManager(this,2,GridLayoutManager.HORIZONTAL,false);
+        mRecyclerView.setLayoutManager(gridLayoutManager);
 
-        recyclerView.setLayoutManager(gridLayoutManager);
-
+        RecipeFitAdapter adapter = new RecipeFitAdapter(this, DataSource.getRecipes(), onItemClickListener);
+        mRecyclerView.setAdapter(adapter);
     }
 }
